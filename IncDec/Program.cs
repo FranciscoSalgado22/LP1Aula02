ab7740ae67d43594b761db4ac77183b35cdc7d02
Francisco Salgado <a21902823@alunos.ulht.pt>
2020-03-05 14:32:00 +0000

    Operaçao de incremento

---
+++ IncDec/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 3, c;
+            c = a++ * b++;
+            Console.WriteLine("Resultado é:" + c);
+        }
+    }
+}
