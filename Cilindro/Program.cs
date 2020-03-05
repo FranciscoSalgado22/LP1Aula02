f743ad6a9ba61ff842a4463e23ba064a13e27474
Francisco Salgado <a21902823@alunos.ulht.pt>
2020-03-05 16:06:07 +0000

    Alteraçao de variaveis por metodos Math 0.2

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,7 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            double pi = 3.1415926, a, r, V, Sa;
+            double a, r, V, Sa;
             string altura, raio;
             Console.WriteLine("Insira a altura:");
             altura = Console.ReadLine();
@@ -15,7 +15,7 @@ namespace Cilindro
             a = double.Parse(altura);
             r = double.Parse(raio);
             V = Math.PI * Math.Pow(r, 2) * a; 
-            Sa = 2 * pi * r * (r + a); 
+            Sa = 2 * Math.PI * r * (r + a); 
             Console.WriteLine("Volume do cilindro: " + V);
             Console.WriteLine("Area superficial: " + Sa);
         }
