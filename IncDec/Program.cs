4cfb909c4b47401511ba041f840874ef1ae4135d
Francisco Salgado <a21902823@alunos.ulht.pt>
2020-03-05 14:36:52 +0000

    Operaçao de incremento em prefixo

--- IncDec/Program.cs
+++ IncDec/Program.cs
@@ -8,7 +8,10 @@ namespace IncDec
         {
             int a = 10, b = 3, c;
             c = a++ * b++;
-            Console.WriteLine("Resultado é:" + c);
+            Console.WriteLine("Resultado de incremento em sufixo: " + c);
+            int d = 10, e = 5, f;
+            f = ++d - ++e;
+            Console.WriteLine("Resultado de incremento em prefixo: " + f);
         }
     }
 }
