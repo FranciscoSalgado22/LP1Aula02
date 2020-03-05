e3c7ee074ffa1dddb7c59de708a856cc69c92282
Francisco Salgado <a21902823@alunos.ulht.pt>
2020-03-05 15:54:47 +0000

    Calculo do volume e area de um cilindro inserindo as variaveis

---
+++ Cilindro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.1415926, a, r, V, Sa;
+            string altura, raio;
+            Console.WriteLine("Insira a altura:");
+            altura = Console.ReadLine();
+            Console.WriteLine("Insira o raio:");
+            raio= Console.ReadLine();
+            a = double.Parse(altura);
+            r = double.Parse(raio);
+            V = pi * (r * r) * a; 
+            Sa = 2 * pi * r * (r + a); 
+            Console.WriteLine("Volume do cilindro: " + V);
+            Console.WriteLine("Area superficial: " + Sa);
+        }
+    }
+}
